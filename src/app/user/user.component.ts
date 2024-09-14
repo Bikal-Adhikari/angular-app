974fadaad7c407b8d3bc1169b84db6f4e142702d
Bikal <bikaladhikari5@gmail.com>
2024-09-14 18:55:52 +1000

    Deleting task complete

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { type User } from './user.model';
 
-type User = {
-  id: string;
-  avatar: string;
-  name: string;
-};
-
-// interface User {
-//   id: string;
-//   avatar: string;
-//   name: string;
-// }
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -19,7 +9,7 @@ type User = {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
-
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
